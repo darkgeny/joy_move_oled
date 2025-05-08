9b879bb69e524c473e590f5d739032e656d03798
darkgeny <117039007+darkgeny@users.noreply.github.com>
2025-05-08 16:38:53 +0200

    added license

--- esp32__joystick_command_2_motor.ino/JoyMove.cpp
+++ esp32__joystick_command_2_motor.ino/JoyMove.cpp
@@ -1,14 +1,28 @@
 /***********************************************************************
-Written by Giacomo Catanzariti darkgeny@gmail.com
-08/05/2025
-License GPL 2.0
-  1. You may copy and distribute verbatim copies of the Program's
-source code as you receive it, in any medium, provided that you
-conspicuously and appropriately publish on each copy an appropriate
-copyright notice and disclaimer of warranty; keep intact all the
-notices that refer to this License and to the absence of any warranty;
-and give any other recipients of the Program a copy of this License
-along with the Program.
+ Name: joy_move_oled
+ Scope: convert joystick movements to motor speed driver and show to oled
+ Copyright (C) 2025 Giacomo Catanzariti
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+    1. You may copy and distribute verbatim copies of the Program's
+  source code as you receive it, in any medium, provided that you
+  conspicuously and appropriately publish on each copy an appropriate
+  copyright notice and disclaimer of warranty; keep intact all the
+  notices that refer to this License and to the absence of any warranty;
+  and give any other recipients of the Program a copy of this License
+  along with the Program.
 ************************************************************************/
 #include "Arduino.h"
 #include "JoyMove.h"
